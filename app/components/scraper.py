ef9209937b893d25bcb4db86fb07a24412eea36d
Jacob Carryer <jacobcarryer@jacobs-mbp.lan>
2021-04-17 19:20:05 -0400

    expanded strenth and casualties

--- app/components/scraper.py
+++ app/components/scraper.py
@@ -28,7 +28,7 @@ class Scraper:
     
     def get_battles(self):
         # get html from wiki page
-        url = "https://en.wikipedia.org/wiki/Battle_of_Fredericksburg"
+        url = "https://en.wikipedia.org/wiki/Skirmish_at_Abingdon"
         r = requests.get(url)
         html_doc = r.text
 
@@ -39,6 +39,7 @@ class Scraper:
 
         # seperate info table
         main_table = soup.find("table", class_="infobox vevent")
+        main_table_rows = main_table.find_all("tr")
 
         # name
         first_header = main_table.find("th") # find first table header, contains battle name
@@ -60,7 +61,6 @@ class Scraper:
             b.locations.append(loc.string)
         
         # belligerents
-        main_table_rows = main_table.find_all("tr")
         for i, row in enumerate(main_table_rows): # loop through rows - side note, im a bit upset i didn't know i could use enumerate() to get the index along with the item when in a for loop like this. this seems very useful
             th = row.find("th") # get header
             if th is not None: # check if header exists
@@ -90,10 +90,14 @@ class Scraper:
             if th is not None:
                 if th.string == "Strength":
                     stren = main_table_rows[i + 1]
-
-                    stren_ps = stren.find_all("p")
-                    for p in stren_ps:
-                        b.strength.append(p.contents[0])
+                    if len(stren.contents[0].contents) > 2:
+                        stren_ps = stren.find_all("p")
+                        for p in stren_ps:
+                            b.strength.append(p.contents[0])
+                    else:
+                        tds = stren.find_all("td")
+                        for td in tds:
+                            b.strength.append(td.contents[0])
         
         # casualties
         for i, row in  enumerate(main_table_rows):
@@ -102,9 +106,14 @@ class Scraper:
                 if th.string == "Casualties and losses":
                     cas = main_table_rows[i + 1]
 
-                    cas_bolds = cas.find_all("b")
-                    for bold in cas_bolds:
-                        b.casualties.append(bold.string)
+                    if len(cas.contents[0].contents) > 2:
+                        cas_bolds = cas.find_all("b")
+                        for bold in cas_bolds:
+                            b.casualties.append(bold.string)
+                    else:
+                        tds = cas.find_all("td")
+                        for td in tds:
+                            b.casualties.append(td.contents[0])
 
 
         # print

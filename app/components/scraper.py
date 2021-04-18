8ffcbc499bc4c4e31f5013997249b68d42fa92da
Jacob Carryer <jacobcarryer@jacobs-mbp.lan>
2021-04-17 20:01:16 -0400

    scraper goes through all battle links

--- app/components/scraper.py
+++ app/components/scraper.py
@@ -20,104 +20,115 @@ class Scraper:
 
         # get links for individual battles and put them into battle_urls
         for link in battle_list_div.find_all("a"):
-            url_p1 = "en.wikipedia.org"
+            url_p1 = "https://en.wikipedia.org"
             url_p2 = link.get("href")
             full_url = url_p1 + url_p2
 
             self.battle_urls.append(full_url)
-    
+        
+        # remove anomoly links
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/List_of_American_Civil_War_battles_in_Northern_Virginia")
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/Manassas_campaign")
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/Battle_of_Clark%27s_House")
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/Battle_of_Smithfield")
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/Joint_Expedition_Against_Franklin")
+        self.battle_urls.remove("https://en.wikipedia.org/wiki/Battle_of_Fairfax_Court_House_(June_1863)")
+
     def get_battles(self):
         # get html from wiki page
-        url = "https://en.wikipedia.org/wiki/Skirmish_at_Abingdon"
-        r = requests.get(url)
-        html_doc = r.text
+        self.get_battle_urls()
+        for url in self.battle_urls:
+            print(url)
 
-        soup = BeautifulSoup(html_doc, "html.parser")
+            r = requests.get(url)
+            print("done")
+            html_doc = r.text
 
-        # create battle object
-        b = Battle()
+            soup = BeautifulSoup(html_doc, "html.parser")
 
-        # seperate info table
-        main_table = soup.find("table", class_="infobox vevent")
-        main_table_rows = main_table.find_all("tr")
+            # create battle object
+            b = Battle()
 
-        # name
-        first_header = main_table.find("th") # find first table header, contains battle name
-        b.name = first_header.string
+            # seperate info table
+            main_table = soup.find("table", class_="infobox vevent")
+            main_table_rows = main_table.find_all("tr")
 
-        # get table and rows containing date/locations/result
-        sub_table = main_table.find("table")
-        sub_table_rows = sub_table.find_all("tr")
+            # name
+            first_header = main_table.find("th") # find first table header, contains battle name
+            b.name = first_header.string
 
-        # date - first row of sub table
-        date = sub_table_rows[0].find("td").string
-        b.date_range = date
+            # get table and rows containing date/locations/result
+            sub_table = main_table.find("table")
+            sub_table_rows = sub_table.find_all("tr")
 
-        # locations - second row of sub table
-        data = sub_table_rows[1].find("td")
-        loc_div = data.find("div", class_="location")
-        locs = loc_div.find_all("a")
-        for loc in locs:
-            b.locations.append(loc.string)
-        
-        # belligerents
-        for i, row in enumerate(main_table_rows): # loop through rows - side note, im a bit upset i didn't know i could use enumerate() to get the index along with the item when in a for loop like this. this seems very useful
-            th = row.find("th") # get header
-            if th is not None: # check if header exists
-                if th.string == "Belligerents":
-                    bellig = main_table_rows[i + 1]
+            # date - first row of sub table
+            date = sub_table_rows[0].find("td").string
+            b.date_range = date
 
-                    a_tags = bellig.find_all("a") # find all a tags and add their text to belligerents
-                    for a in a_tags:
-                        if a.string is not None:
-                            b.belligerents.append(a.string)
-        
-        # leaders
-        for i, row in enumerate(main_table_rows):
-            th = row.find("th")
-            if th is not None:
-                if th.string == "Commanders and leaders":
-                    lead = main_table_rows[i + 1]
+            # locations - second row of sub table
+            data = sub_table_rows[1].find("td")
+            loc_div = data.find("div", class_="location")
+            locs = loc_div.find_all("a")
+            for loc in locs:
+                b.locations.append(loc.string)
+            
+            # belligerents
+            for i, row in enumerate(main_table_rows): # loop through rows - side note, im a bit upset i didn't know i could use enumerate() to get the index along with the item when in a for loop like this. this seems very useful
+                th = row.find("th") # get header
+                if th is not None: # check if header exists
+                    if th.string == "Belligerents" or th.string == "Units involved":
+                        bellig = main_table_rows[i + 1]
 
-                    a_tags = lead.find_all("a")
-                    for a in a_tags:
-                        if a.string is not None:
-                            b.leaders.append(a.string)
-        
-        # strength
-        for i, row in enumerate(main_table_rows):
-            th = row.find("th")
-            if th is not None:
-                if th.string == "Strength":
-                    stren = main_table_rows[i + 1]
-                    if len(stren.contents[0].contents) > 2:
-                        stren_ps = stren.find_all("p")
-                        for p in stren_ps:
-                            b.strength.append(p.contents[0])
-                    else:
-                        tds = stren.find_all("td")
-                        for td in tds:
-                            b.strength.append(td.contents[0])
-        
-        # casualties
-        for i, row in  enumerate(main_table_rows):
-            th = row.find("th")
-            if th is not None:
-                if th.string == "Casualties and losses":
-                    cas = main_table_rows[i + 1]
+                        a_tags = bellig.find_all("a") # find all a tags and add their text to belligerents
+                        for a in a_tags:
+                            if a.string is not None:
+                                b.belligerents.append(a.string)
+            
+            # leaders
+            for i, row in enumerate(main_table_rows):
+                th = row.find("th")
+                if th is not None:
+                    if th.string == "Commanders and leaders":
+                        lead = main_table_rows[i + 1]
 
-                    if len(cas.contents[0].contents) > 2:
-                        cas_bolds = cas.find_all("b")
-                        for bold in cas_bolds:
-                            b.casualties.append(bold.string)
-                    else:
-                        tds = cas.find_all("td")
-                        for td in tds:
-                            b.casualties.append(td.contents[0])
+                        a_tags = lead.find_all("a")
+                        for a in a_tags:
+                            if a.string is not None:
+                                b.leaders.append(a.string)
+            
+            # strength
+            for i, row in enumerate(main_table_rows):
+                th = row.find("th")
+                if th is not None:
+                    if th.string == "Strength":
+                        stren = main_table_rows[i + 1]
+                        if len(stren.contents[0].contents) > 2:
+                            stren_ps = stren.find_all("p")
+                            for p in stren_ps:
+                                b.strength.append(p.contents[0])
+                        else:
+                            tds = stren.find_all("td")
+                            for td in tds:
+                                b.strength.append(td.contents[0])
+            
+            # casualties
+            for i, row in  enumerate(main_table_rows):
+                th = row.find("th")
+                if th is not None:
+                    if th.string == "Casualties and losses":
+                        cas = main_table_rows[i + 1]
+
+                        if len(cas.contents[0].contents) > 2:
+                            cas_bolds = cas.find_all("b")
+                            for bold in cas_bolds:
+                                b.casualties.append(bold.string)
+                        else:
+                            tds = cas.find_all("td")
+                            for td in tds:
+                                b.casualties.append(td.contents[0])
 
 
-        # print
-        b.print_battle()
+            self.battles.append(b)
 
 
 
@@ -125,3 +136,5 @@ class Scraper:
 # test code
 scraper = Scraper()
 scraper.get_battles()
+for b in scraper.battles:
+    b.print_battle()
